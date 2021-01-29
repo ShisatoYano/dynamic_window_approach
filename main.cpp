f1f5c2f50654b837b94396639ecf2329264da2ac
Shisato <shisatoyano@gmail.com>
2021-01-29 21:59:39 +0900

    defined state structure

--- main.cpp
+++ main.cpp
@@ -1,4 +1,14 @@
-#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct State {
+    float x_m;
+    float y_m;
+    float yaw_rad;
+    float v_ms;
+    float omega_rads;
+};
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
